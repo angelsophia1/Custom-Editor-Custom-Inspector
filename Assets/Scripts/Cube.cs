82f876a57fcb5a1f3bc94fe8d1c221c183b58d13
angelsophia1 <angelsunyulei@126.com>
2019-07-25 18:56:50 +0800

    Initial commit

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    public void GenerateColor()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = Random.ColorHSV();
+    }
+    public void Reset()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = Color.white;
+    }
+}
